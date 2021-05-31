9a687970468c03bf513b3d72d9405d4c6583326c
maxlech974 <maximelechere.dev@gmail.com>
2021-05-31 18:41:36 +0200

    mise en place d'api platform, création des 3 entités (user, todolist, task), authentification, création des fixtures

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -14,4 +14,7 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
