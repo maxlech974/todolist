f819fd64703950c26f417a6b92dacebd1fb58b60
maxlech974 <maximelechere.dev@gmail.com>
2021-06-04 18:32:46 +0200

    card ok pour les tâches

--- front/src/components/Tasks/index.js
+++ front/src/components/Tasks/index.js
@@ -17,24 +17,36 @@ const Tasks = ({ tasksList }) => {
   const tasksByDate = tasksList.sort(compare);
   const taskActive = [];
   const tasksDone = [];
+  console.log(tasksDone.length)
   tasksByDate.forEach( e => {
     (e.isFinished) ? tasksDone.push(e) : taskActive.push(e)
   })
 
   console.log('tâches en cours', taskActive)
+  console.log('tâches terminées', tasksDone);
   
   return(
     <Container className="task-container">
       {taskActive.map((task)=>(
-      <Card className="task">
-        <Card.Body>
-          <Card.Text className="task-tittle">
-            {task.name}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>{getDate(task.createdAt)}</Card.Footer>
-      </Card>
-      ))}
+        <Card className="task" key={task.id}>
+          <Card.Body>
+            <Card.Text className="task-tittle">
+              {task.name}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>{getDate(task.createdAt)}</Card.Footer>
+        </Card>
+        ))}
+      {tasksDone.map((task)=>(
+          <Card className="task__done" key={task.id}>
+          <Card.Body className="task-body">
+            <Card.Text className="task-tittle">
+              {task.name}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer className="task-footer">{getDate(task.createdAt)}</Card.Footer>
+        </Card>
+        ))}
     </Container>
   )
 }
